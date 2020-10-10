e7386f5e2fe963a2b62611efd4aa836ab46dd126
aligator <aligator@suncraft-server.de>
2020-10-10 23:24:53 +0200

    init

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Debug.Log("GameHandler.Start");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
