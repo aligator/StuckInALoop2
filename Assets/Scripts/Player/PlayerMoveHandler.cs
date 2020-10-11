7f4146a5c40c7065a1cf4ce85390febdc374db00
aligator <aligator@suncraft-server.de>
2020-10-11 21:43:53 +0200

    Restrict player moves to camera

--- Assets/Scripts/Player/PlayerMoveHandler.cs
+++ Assets/Scripts/Player/PlayerMoveHandler.cs
@@ -1,24 +1,42 @@
-﻿using UnityEngine;
+﻿using System.Data;
+using UnityEngine;
 
 namespace Player {
     public class PlayerMoveHandler : MonoBehaviour {
         private const float Speed = 0.1f;
 
         private Rigidbody2D body;
+        private Vector2 max;
+
+        private Vector2 min;
 
         private void Start() {
             body = GetComponent<Rigidbody2D>();
+            var cam = Camera.main;
+
+            if (cam == null) throw new NoNullAllowedException();
+
+            var playerSize = GetComponent<SpriteRenderer>().sprite.bounds.size;
+
+            min = cam.ViewportToWorldPoint(new Vector3()) + playerSize / 2;
+            max = cam.ViewportToWorldPoint(cam.rect.size) - playerSize / 2;
         }
 
         private void FixedUpdate() {
-            var transformPosition = transform.position;
+            var newPosition = transform.position;
 
-            if (Input.GetKey(KeyCode.W)) transformPosition.y += Speed;
-            if (Input.GetKey(KeyCode.S)) transformPosition.y -= Speed;
-            if (Input.GetKey(KeyCode.A)) transformPosition.x -= Speed;
-            if (Input.GetKey(KeyCode.D)) transformPosition.x += Speed;
+            if (Input.GetKey(KeyCode.W)) newPosition.y += Speed;
+            if (Input.GetKey(KeyCode.S)) newPosition.y -= Speed;
+            if (Input.GetKey(KeyCode.A)) newPosition.x -= Speed;
+            if (Input.GetKey(KeyCode.D)) newPosition.x += Speed;
 
-            body.MovePosition(transformPosition);
+            if (newPosition.x < min.x) newPosition.x = min.x;
+            if (newPosition.x > max.x) newPosition.x = max.x;
+
+            if (newPosition.y < min.y) newPosition.y = min.y;
+            if (newPosition.y > max.y) newPosition.y = max.y;
+
+            body.MovePosition(newPosition);
         }
     }
 }
